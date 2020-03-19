f36629ee88bb86a5209de739dd4a6aabf802c5ea
ParkSeonbi <55050662+ParkSeonbi@users.noreply.github.com>
2020-03-19 23:50:56 +0900

    udemy flutter themedata custom widget

---
+++ lib/input_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1d1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1d1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: Color(0xFF1d1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1d1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1d1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+//      floatingActionButton: Theme(
+//        data: ThemeData(accentColor: Colors.purple),
+//        child: FloatingActionButton(
+//          child: Icon(Icons.add),
+//        ),
+//      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+  final Color colour;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
